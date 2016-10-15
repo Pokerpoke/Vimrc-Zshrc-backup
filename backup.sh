eabbaec3c132aebffdfa24d2d10aa6dda763a252
pokerpoke <j824544269@gmail.com>
2016-10-15 05:00:10 +0000

    Backup  !

---
+++ backup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cp ~/.vimrc ./vimrc.bak
+echo "Backup vimrc succed!"
+cp ~/.zshrc ./zshrc.bak
+echo "Backup zshrc succed!"
+
+git add .
+date1=$(date +%Y%m%d)
+echo "$date1"
+git commit -m "Backup $date !"
+git push
+echo "Git push succed!"
