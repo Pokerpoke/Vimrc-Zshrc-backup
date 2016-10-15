a73ce3895a8a2228aeeebd5677493926f51b172b
pokerpoke <j824544269@gmail.com>
2016-10-15 05:57:19 -0400

    20161015 Digital Ocean

---
+++ restore.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cp ./zshrc.bak ~/.zshrc
+echo "Restore zshrc succeed!"
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+echo "Vundle has been installed!"
+
+cp ./vimrc.bak ~/.vimrc
+echo "Restore vimrc succeed!"
