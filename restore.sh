2104ab960cd7d83d68c74a7ae23c156abcf1d5c4
pokerpoke <j824544269@gmail.com>
2017-03-30 19:42:11 +0800

    repair

--- restore.sh
+++ restore.sh
@@ -9,10 +9,8 @@ case $IFLAG in
 	N | n )
 		echo "oh-my-zsh won't install";;
 	* )
-		echo "error choice!"
+		echo "error choice!";;
 esac
-
-<<<<<<< HEAD
 
 # Restore zshrc
 read -p "Restore zshrc? [Y/N]" IFLAG
@@ -20,11 +18,11 @@ read -p "Restore zshrc? [Y/N]" IFLAG
 case $IFLAG in
 	Y | y )
 		cp ./zshrc.bak ~/.zshrc
-		echo "Restore zshrc succeed!"
+		echo "Restore zshrc succeed!";;
 	N | n )
 		echo "zshrc won't Restore";;
 	* )
-		echo "error choice!"
+		echo "error choice!";;
 esac
 
 # Install Vundle
@@ -33,11 +31,11 @@ read -p "Install Vundle? [Y/N]" IFLAG
 case $IFLAG in
 	Y | y )
 		git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-		echo "Vundle has been installed!"
+		echo "Vundle has been installed!";;
 	N | n )
 		echo "Vundle won't install";;
 	* )
-		echo "error choice!"
+		echo "error choice!";;
 esac
 
 # Install Vundle
@@ -46,14 +44,9 @@ read -p "Restore vimrc? [Y/N]" IFLAG
 case $IFLAG in
 	Y | y )
 		cp ./vimrc.bak ~/.vimrc
-		echo "Restore vimrc succeed!"
+		echo "Restore vimrc succeed!";;
 	N | n )
 		echo "vimrc won't Restore";;
 	* )
-		echo "error choice!"
+		echo "error choice!";;
 esac
-
-=======
-cp ./vimrc.bak ~/.vimrc
-echo "Restore vimrc succeed!"
->>>>>>> 45dfcf621885f9d8af4c8535b45a2e81c76030fb
