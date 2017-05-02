7dfe5a5501e129125f359504c2233e0d79f1cbc5
pokerpke <j824544269@gmail.com>
2017-05-03 01:04:45 +0800

    add support applications.add vscode and more applications.

--- README.md
+++ README.md
@@ -1,19 +1,21 @@
 # Vimrc-Zshrc-backup
 
 Backup your vimrc and zshrc and restore at a new installed computer.
-* ~~You need to change the name of user in the zshrc.bak otherwise it will get an error.~~
-* ~~The installapp.sh should be run as root.~~  
+* ~~*You need to change the name of user in the zshrc.bak otherwise it will get an error.*~~
+* ~~*The installapp.sh should be run as root.*~~  
 Don't run the script as run or the .vimrc and .zshrc will own to root.
 
-* [x] Add commit message
-* [x] Add date when commit
-* [x] No need to change the name of user when restore zshrc.bak
-* [x] Auto git push when run backup.sh
-* [x] Ask before backup
-* [x] Ask before restore
+* [X] ~~*Add commit message*~~
+* [X] ~~*Add date when commit*~~
+* [X] ~~*No need to change the name of user when restore zshrc.bak*~~
+* [X] ~~*Auto git push when run backup.sh*~~
+* [X] ~~*Ask before backup*~~
+* [X] ~~*Ask before restore*~~
+* [ ] Add an install all choice
 * [ ] No need to run PluginInstall in vim
-* [ ] Auto skip installed appliacation
-* [ ] No effection to origin/master when backup
+* [ ] Auto skip installed appliacations
+* [ ] No effection to `origin/master` when backup
+* [ ] Maybe it can be a gui
 * [ ] More
 
 # Usage
@@ -33,3 +35,30 @@ git clone https://github.com/Pokerpoke/Vimrc-Zshrc-backup
 git clone https://github.com/Pokerpoke/Vimrc-Zshrc-backup
 ./installapp.sh
 ```
+
+# Support applications
+* chromium-browser
+* git
+* zsh
+* vim
+* htop
+* tree
+* uget
+* unity-tweak-tool
+* indicator-multiload
+* cmake
+* powerline
+* synaptic
+* curl
+* aria2
+* steam
+* vlc
+* shadowsocks-qt5
+* arc-theme
+* albert
+* sublimetext3
+* powerline fonts
+* vscode
+* numix icon theme
+* sogou input
+* oh-my-zsh
--- installapp.sh
+++ installapp.sh
@@ -7,7 +7,7 @@ fuser=$USER
 
 
 apps=(
-	"chromium\ browser"
+	"chromium-browser"
 	"git"
 	"zsh"
 	"vim"
@@ -18,6 +18,10 @@ apps=(
 	"indicator-multiload"
 	"cmake"
 	"powerline"
+	"synaptic"
+	"curl"
+	"aria2"
+	"steam"
 	)
 
 for name in ${apps[@]}; do
@@ -35,6 +39,18 @@ for name in ${apps[@]}; do
 	esac
 	
 done
+
+#install vlc
+read -p "install vlc? [Y/N]" IFLAG
+
+case $IFLAG in
+	Y | y )
+		sudo apt-get install -y vlc browser-plugin-vlc;;
+	N | n )
+		echo "shadowsocks-qt5 won't install!";;
+	* )
+		echo "error choice!";;
+esac
 
 #install shadowsocks-qt5
 read -p "install shadowsocks-qt5? [Y/N]" IFLAG
@@ -55,8 +71,9 @@ read -p "install arc-theme? [Y/N]" IFLAG
 
 case $IFLAG in
 	Y | y )
-		sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' >> /etc/apt/sources.list.d/arc-theme.list"
-		sudo apt-get update && sudo apt-get install arc-theme;;
+		sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' >> /etc/apt/sources.list.d/arc-theme.list"
+		sudo apt-get update 
+		sudo apt-get install -y arc-theme;;
 	N | n )
 		echo "arc-theme won't install!";;
 	* )
@@ -70,7 +87,7 @@ case $IFLAG in
 	Y | y )
 		sudo add-apt-repository ppa:nilarimogard/webupd8
 		sudo apt-get update
-		sudo apt-get install albert;;
+		sudo apt-get install -y albert;;
 	N | n )
 		echo "albert won't install!";;
 	* )
@@ -107,7 +124,53 @@ case $IFLAG in
 		rm -rf fonts
 		echo "install success!";;
 	N | n )
-		echo "sublimetext3 won't install!";;
+		echo "powerline fonts won't install!";;
+	* )
+		echo "error choice!";;
+esac
+
+#install vscode
+read -p "install vscode? [Y/N]" IFLAG
+
+case $IFLAG in
+	Y | y )
+		curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+		sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+		sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+		sudo apt update
+		sudo apt-get install code # or code-insiders
+		echo "install success!";;
+	N | n )
+		echo "vscode won't install!";;
+	* )
+		echo "error choice!";;
+esac
+
+#install numix icon theme
+read -p "install numix icon theme? [Y/N]" IFLAG
+
+case $IFLAG in
+	Y | y )
+		sudo add-apt-repository ppa:numix/ppa
+		sudo apt-get update
+		sudo apt-get install numix-icon-theme
+		echo "install success!";;
+	N | n )
+		echo "vscode won't install!";;
+	* )
+		echo "error choice!";;
+esac
+
+#install sogou input
+read -p "install sougou input? [Y/N]" IFLAG
+
+case $IFLAG in
+	Y | y )
+		sudo add-apt-repository ppa:fcitx-team/nightly  #添加源
+		sudo apt-get install fcitx sogoupinyin          #安装 fcitx 和搜狗拼音
+		echo "install success!";;
+	N | n )
+		echo "sogou input won't install!";;
 	* )
 		echo "error choice!";;
 esac
